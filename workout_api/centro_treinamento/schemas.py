91fc3afce0c05c7550f15e77271177ba15cfea6b
Bbvmex <hugovelasmar@gmail.com>
2024-06-14 01:19:45 -0300

    Criação das rotas de CT

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -1,10 +1,17 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import UUID4, Field
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do centro de treinamento', example='Academia Fly', max_length=20)]
     endereco: Annotated[str, Field(description='Endereço completo do centro de treinamento', example='Rua exemplo, 10, bairro exemplo - Cidade/SP', max_length=60)]
     proprietario: Annotated[str, Field(description='Nome do proprietário do centro de treinamento', example='Marcos Paulo', max_length=30)]
+  
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='Academia Fly', max_length=20)]
+    
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]
+
